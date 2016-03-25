22ab75cdacdca704751cd0279844528d90031f2b
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-25 18:08:22 +0600

    [fix] xpathes in Authorisation class

--- src/test/java/com/epam/at/web_driver_task/AuthorisationTest.java
+++ src/test/java/com/epam/at/web_driver_task/AuthorisationTest.java
@@ -3,16 +3,29 @@ package com.epam.at.web_driver_task;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class AuthorisationTest {
     private WebDriver driver = WebDriverFactory.firefoxInstance();
     private String login;
     private String password;
+    private static final String YANDEX_URL = "https://www.yandex.kz/";
 
-    @Factory(dataProvider = "testAccountCredentials")
+    @BeforeClass(alwaysRun = true)
+    public static void startFirefox() {
+        WebDriverFactory.firefoxInstance().get(YANDEX_URL);
+    }
+
+    /*@AfterClass(alwaysRun = true)
+    public static void quitFirefox() {
+        WebDriverFactory.firefoxInstance().close();
+    }*/
+
+    @Factory(dataProvider = "testAccountCredentials", dataProviderClass = MailDataProvider.class)
     public AuthorisationTest(String login, String password) {
         this.login = login;
         this.password = password;
@@ -20,16 +33,19 @@ public class AuthorisationTest {
 
     @Test
     public void loginSuccessIfMailAndPasswordAreCorrect() {
-        driver.findElement(By.xpath("//*[@href='https://mail.yandex.kz/']")).click();
-        driver.findElement(By.className("login")).sendKeys(login);
-        driver.findElement(By.className("passwd")).sendKeys(password);
-        driver.findElement(By.className(" nb-button _nb-action-button nb-group-start")).click();
-        Assert.assertTrue(driver.getCurrentUrl().endsWith("uid=373236611&login=test1441#inbox"));
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//input[@name=\"login\"]")).sendKeys(login);
+        driver.findElement(By.xpath("//input[@name=\"passwd\"]")).sendKeys(password);
+        driver.findElement(By.xpath("//div[@class=\"domik2__submit\"]")).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().refresh();
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertTrue(driver.getCurrentUrl().contains("uid=373236611&login=test1441#inbox"));
     }
 
-    @AfterTest
+   /* @AfterTest
     public void logoffSuccess() {
         driver.findElement(By.xpath("//a[@id='nb-1']")).click();
         driver.findElement(By.xpath("//a[@data-metric=\"Меню сервисов:Выход\"]")).click();
-    }
+    }*/
 }
--- src/test/java/com/epam/at/web_driver_task/WebDriverFactory.java
+++ src/test/java/com/epam/at/web_driver_task/WebDriverFactory.java
@@ -1,35 +1,17 @@
 package com.epam.at.web_driver_task;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
 
 public class WebDriverFactory {
     private WebDriverFactory() {
     }
 
-    public static class WebDriverInstancesHolder {
+    public static class WebDriverInstanceHolder {
         public static final WebDriver FIREFOX_INSTANCE = new FirefoxDriver();
-        public static final WebDriver CHROME_INSTANCE = new ChromeDriver();
-        public static final WebDriver OPERA_INSTANCE = new OperaDriver();
-        public static final WebDriver EXPLORER_INSTANCE = new InternetExplorerDriver();
     }
 
     public static WebDriver firefoxInstance() {
-        return WebDriverInstancesHolder.FIREFOX_INSTANCE;
-    }
-
-    public static WebDriver chromeInstance() {
-        return WebDriverInstancesHolder.CHROME_INSTANCE;
-    }
-
-    public static WebDriver operaInstance() {
-        return WebDriverInstancesHolder.OPERA_INSTANCE;
-    }
-
-    public static WebDriver explorerInstance() {
-        return WebDriverInstancesHolder.EXPLORER_INSTANCE;
+        return WebDriverInstanceHolder.FIREFOX_INSTANCE;
     }
 }
