b0a1bf180517cecbe1c1ccb2515a53fdca8a71c9
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-28 14:14:45 +0600

    [fix] add fluent wait in draftCreateWithContentAndSave() to escape repeating save alert

--- src/test/java/com/epam/at/web_driver_task/DraftCreation.java
+++ src/test/java/com/epam/at/web_driver_task/DraftCreation.java
@@ -1,8 +1,14 @@
 package com.epam.at.web_driver_task;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class DraftCreation extends MailScenario {
     private String to;
@@ -17,18 +23,18 @@ public class DraftCreation extends MailScenario {
     }
 
     @Test
-    public void draftCreateWithContentAndSave() {
+    public void draftCreateWithContentAndSave() throws InterruptedException {
         driver.findElement(By.xpath("//a[@href=\"#compose\"]")).click();
         driver.findElement(By.xpath("//div[@class=\"b-mail-input__yabbles\"]/div/input")).sendKeys(to);
         driver.findElement(By.xpath("id(\"compose-subj\")")).sendKeys(subject);
         driver.findElement(By.xpath("id(\"compose-send_ifr\")")).sendKeys(message);
         driver.findElement(By.xpath("//a[@href=\"#inbox\"]")).click();
         driver.findElement(By.xpath("//div[@class=\"b-popup__confirm\"]/button")).click();
-    }
-
-
-    @Test
-    public void draftCheckIsSaved() {
-
+        Thread.sleep(2000);
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(5, TimeUnit.SECONDS)
+                .pollingEvery(1, TimeUnit.SECONDS);
+        wait.until(webDriver -> driver.getCurrentUrl().endsWith("#inbox"));
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("#inbox"));
     }
 }
