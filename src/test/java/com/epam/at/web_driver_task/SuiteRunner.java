a1e4f2b5db5df46703b9d0b265ff130054e01f89
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-29 10:17:56 +0600

    [fix] remove @Before/@After methods

--- src/test/java/com/epam/at/web_driver_task/SuiteRunner.java
+++ src/test/java/com/epam/at/web_driver_task/SuiteRunner.java
@@ -1,8 +1,6 @@
 package com.epam.at.web_driver_task;
 
 import org.testng.TestNG;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.xml.Parser;
 import org.testng.xml.XmlSuite;
 import org.xml.sax.SAXException;
@@ -12,18 +10,6 @@ import java.io.IOException;
 import java.util.List;
 
 public class SuiteRunner {
-    private static final String YANDEX_URL = "https://www.yandex.kz/";
-
-    @BeforeSuite(alwaysRun = true)
-    public static void startFirefox() {
-        WebDriverFactory.firefoxInstance().get(YANDEX_URL);
-    }
-
-    @AfterSuite(alwaysRun = true)
-    public static void quitFirefox() {
-        WebDriverFactory.firefoxInstance().close();
-    }
-
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
         TestNG tng = new TestNG(true);
         final Parser parser = new Parser("src/test/resources/suite.xml");
