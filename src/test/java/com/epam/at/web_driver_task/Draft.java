9579fa917dbcdedc4611bcecbd289e4120431607
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-29 10:02:17 +0600

    [imp] add method for draft content check

--- src/test/java/com/epam/at/web_driver_task/Draft.java
+++ src/test/java/com/epam/at/web_driver_task/Draft.java
@@ -2,6 +2,7 @@ package com.epam.at.web_driver_task;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
@@ -45,5 +46,21 @@ public class Draft extends MailScenario {
         wait.until(webDriver -> driver.getCurrentUrl().endsWith("#inbox"));
         driver.get(driver.getCurrentUrl().replace("#inbox", "#draft"));
         Assert.assertTrue(driver.findElement(By.xpath("//div[@class=\"block-messages-wrap\"]/div[@class=\"b-messages\"]/div[1]")) != null);
+    }
+
+
+    @Test(priority = 2, enabled = false)
+    public void checkDraftContent() throws InterruptedException {
+        driver.findElement(By.xpath("//div[@class=\"block-messages-wrap\"]/div[@class=\"b-messages\"]/div[1]")).click();
+        Thread.sleep(5000);
+        String toActual = driver.findElement(By.xpath("//div[@class=\"b-mail-input__yabbles\"]/div/span/span[last()]")).getText();
+        String subjectActual = driver.findElement(By.xpath("//div[@class=\"b-input\"]/descendant::div")).getText();
+
+        WebElement element = driver.findElement(By.xpath("//table[@id=\"compose-send_tbl\"]/descendant::tr[@class=\"mceLast\"]/td/iframe"));
+        element.click();
+        String messageActual = element.findElement(By.xpath("//*[@id='tinymce']/div[1]")).getText();
+        System.out.println(toActual);
+        System.out.println(subjectActual);
+        System.out.println(messageActual);
     }
 }
