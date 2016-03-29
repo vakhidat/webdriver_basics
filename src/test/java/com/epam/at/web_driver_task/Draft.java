34913f90367b673cd7ecba5edd4368ecc7751719
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-03-29 10:00:39 +0600

    [imp] add method for draft in folder check

--- src/test/java/com/epam/at/web_driver_task/Draft.java
+++ src/test/java/com/epam/at/web_driver_task/Draft.java
@@ -10,31 +10,40 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class DraftCreation extends MailScenario {
+public class Draft extends MailScenario {
     private String to;
     private String subject;
     private String message;
 
     @Factory(dataProvider = "draftMailContentAndRecipientMail", dataProviderClass = MailDataProvider.class)
-    public DraftCreation(String to, String subject, String message) {
+    public Draft(String to, String subject, String message) {
         this.to = to;
         this.subject = subject;
         this.message = message;
     }
 
-    @Test
-    public void draftCreateWithContentAndSave() throws InterruptedException {
+    @Test(priority = 0)
+    public void draftCreateWithContentAndSave() {
         driver.findElement(By.xpath("//a[@href=\"#compose\"]")).click();
         driver.findElement(By.xpath("//div[@class=\"b-mail-input__yabbles\"]/div/input")).sendKeys(to);
         driver.findElement(By.xpath("id(\"compose-subj\")")).sendKeys(subject);
         driver.findElement(By.xpath("id(\"compose-send_ifr\")")).sendKeys(message);
         driver.findElement(By.xpath("//a[@href=\"#inbox\"]")).click();
         driver.findElement(By.xpath("//div[@class=\"b-popup__confirm\"]/button")).click();
-        Thread.sleep(2000);
         Wait<WebDriver> wait = new FluentWait<>(driver)
                 .withTimeout(5, TimeUnit.SECONDS)
                 .pollingEvery(1, TimeUnit.SECONDS);
         wait.until(webDriver -> driver.getCurrentUrl().endsWith("#inbox"));
         Assert.assertTrue(driver.getCurrentUrl().endsWith("#inbox"));
+    }
+
+    @Test(priority = 1)
+    public void checkDraftIsPresentInDraftFolder() {
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(5, TimeUnit.SECONDS)
+                .pollingEvery(1, TimeUnit.SECONDS);
+        wait.until(webDriver -> driver.getCurrentUrl().endsWith("#inbox"));
+        driver.get(driver.getCurrentUrl().replace("#inbox", "#draft"));
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class=\"block-messages-wrap\"]/div[@class=\"b-messages\"]/div[1]")) != null);
     }
 }
