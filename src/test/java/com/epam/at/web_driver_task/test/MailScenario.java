955783361e161bd8c116d540740371c901ef9b4b
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-04-04 17:31:29 +0600

    [fix] remove webdriver factory

--- src/test/java/com/epam/at/web_driver_task/test/MailScenario.java
+++ src/test/java/com/epam/at/web_driver_task/test/MailScenario.java
@@ -1,27 +1,25 @@
 package com.epam.at.web_driver_task.test;
 
-import com.epam.at.web_driver_task.WebDriverFactory;
 import com.epam.at.web_driver_task.page.Inbox;
 import com.epam.at.web_driver_task.page.MailPage;
 import com.epam.at.web_driver_task.page.Main;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import static com.epam.at.web_driver_task.WebDriverFactory.firefoxDriver;
-
 public abstract class MailScenario {
-    protected static WebDriver driver = WebDriverFactory.firefoxDriver();
+    protected static WebDriver driver = new FirefoxDriver();
     protected static Main mainPage = new Main(driver);
     protected static MailPage mailPage = new Inbox(driver);
 
     @BeforeSuite(alwaysRun = true)
     public static void startFirefox() {
-        firefoxDriver().get(Main.YANDEX_URL);
+        driver.get(Main.YANDEX_URL);
     }
 
     @AfterSuite(alwaysRun = true)
     public static void quitFirefox() {
-        firefoxDriver().close();
+        driver.close();
     }
 }
