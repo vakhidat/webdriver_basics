622a273b1fae2e829655783847e0838c4d3dd451
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-04-08 18:06:28 +0600

    [fix] fix report util

--- src/main/java/com/epam/at/web_driver_task/util/ReportUtil.java
+++ src/main/java/com/epam/at/web_driver_task/util/ReportUtil.java
@@ -5,19 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public final class ReportUtil {
+
+    public static final String DEFAULT_HIGHLIGHT_COLOR = "yellow";
+
     public static void highlightElement(WebDriver driver, WebElement element) {
+        String backgroundColor = element.getCssValue("backgroundColor");
+        startHighlightElement(driver, element, DEFAULT_HIGHLIGHT_COLOR);
+        stopHighlightElement(driver, element, backgroundColor);
+    }
+
+    public static void highlightElementAndTakeScreenshot(WebDriver driver, WebElement element, String screenshotPrefix, String color) {
         String bg = element.getCssValue("backgroundColor");
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("arguments[0].style.backgroundColor = '" + "yellow" + "'", element);
-        js.executeScript("arguments[0].style.backgroundColor = '" + bg + "'", element);
+        startHighlightElement(driver, element, color);
+        ScreenshotUtils.makeScreenshot(driver, screenshotPrefix);
+        stopHighlightElement(driver, element, bg);
     }
 
     public static void highlightElementAndTakeScreenshot(WebDriver driver, WebElement element, String screenshotPrefix) {
-        String bg = element.getCssValue("backgroundColor");
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("arguments[0].style.backgroundColor = '" + "red" + "'", element);
-        ScreenshotUtils.makeScreenshot(driver, screenshotPrefix);
-        js.executeScript("arguments[0].style.backgroundColor = '" + bg + "'", element);
+        highlightElementAndTakeScreenshot(driver, element, screenshotPrefix, "red");
     }
 
     public static void highlightElementAndTakeScreenshot(WebDriver driver, WebElement element) {
@@ -26,5 +31,15 @@ public final class ReportUtil {
         js.executeScript("arguments[0].style.backgroundColor = '" + "red" + "'", element);
         ScreenshotUtils.makeScreenshot(driver);
         js.executeScript("arguments[0].style.backgroundColor = '" + bg + "'", element);
+    }
+
+    private static void startHighlightElement(WebDriver driver, WebElement element, String color) {
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("arguments[0].style.backgroundColor = '" + color + "'", element);
+    }
+
+    private static void stopHighlightElement(WebDriver driver, WebElement element, String initialBackgroundColor) {
+        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        js.executeScript("arguments[0].style.backgroundColor = '" + initialBackgroundColor + "'", element);
     }
 }
