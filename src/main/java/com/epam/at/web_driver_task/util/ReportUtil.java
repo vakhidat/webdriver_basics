d49f98409a8cc8c35de5f37e1436cfae83e2c422
Vakhidat Matiyeva <vahidat.m@yandex.ru>
2016-04-11 18:02:06 +0600

    [ref] report utils

--- src/main/java/com/epam/at/web_driver_task/util/ReportUtil.java
+++ src/main/java/com/epam/at/web_driver_task/util/ReportUtil.java
@@ -5,8 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public final class ReportUtil {
-
-    public static final String DEFAULT_HIGHLIGHT_COLOR = "yellow";
+    private static final String DEFAULT_HIGHLIGHT_COLOR = "yellow";
+    private static final String DEFAULT_HIGHLIGHT = "red";
+    public static final String DEFAULT_SCREENSHOT_PREFIX = "hl-el";
 
     public static void highlightElement(WebDriver driver, WebElement element) {
         String backgroundColor = element.getCssValue("backgroundColor");
@@ -15,22 +16,18 @@ public final class ReportUtil {
     }
 
     public static void highlightElementAndTakeScreenshot(WebDriver driver, WebElement element, String screenshotPrefix, String color) {
-        String bg = element.getCssValue("backgroundColor");
+        String elementBackgroundColor = element.getCssValue("backgroundColor");
         startHighlightElement(driver, element, color);
         ScreenshotUtils.makeScreenshot(driver, screenshotPrefix);
-        stopHighlightElement(driver, element, bg);
+        stopHighlightElement(driver, element, elementBackgroundColor);
     }
 
     public static void highlightElementAndTakeScreenshot(WebDriver driver, WebElement element, String screenshotPrefix) {
-        highlightElementAndTakeScreenshot(driver, element, screenshotPrefix, "red");
+        highlightElementAndTakeScreenshot(driver, element, screenshotPrefix, DEFAULT_HIGHLIGHT);
     }
 
     public static void highlightElementAndTakeScreenshot(WebDriver driver, WebElement element) {
-        String bg = element.getCssValue("backgroundColor");
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
-        js.executeScript("arguments[0].style.backgroundColor = '" + "red" + "'", element);
-        ScreenshotUtils.makeScreenshot(driver);
-        js.executeScript("arguments[0].style.backgroundColor = '" + bg + "'", element);
+        highlightElementAndTakeScreenshot(driver, element, DEFAULT_SCREENSHOT_PREFIX, DEFAULT_HIGHLIGHT);
     }
 
     private static void startHighlightElement(WebDriver driver, WebElement element, String color) {
